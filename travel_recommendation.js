af0dcac44107b221c21156ce1cdd00e2224aef10
gosxiesperanto <g.toyota@gmail.com>
2026-02-20 11:37:03 -0500

    reset added

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,14 +1,13 @@
 function search(){
-        const word document.getElementById("text").toLowerCase;
+        const word = document.getElementById("txt").toLowerCase;
         word.innerHTML = result;
 
-        fetch('./travel_recommendation.json')
+        fetch('./travel_recommendation_api.json')
           .then(response => response.json())
           .then(data => console.log(data))
           .catch(error => console.error('Error:', error));
       }
 
-function reset(text){
-        const cleared document.getElementById("result").innerHTML = "";
-        text = "";
+function reset(){
+    txt.value = "";
         }
