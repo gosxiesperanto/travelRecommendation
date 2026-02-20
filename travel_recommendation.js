c5b7c8c9c184947984aa6c44206818a10f7a233b
gosxiesperanto <g.toyota@gmail.com>
2026-02-20 05:40:06 -0500

    pages added

---
+++ travel_recommendation.js
@@ -0,0 +1,13 @@
+function search(){
+        const word document.getElementById("text").toLowerCase;
+        word.innerHTML = result;
+
+        fetch('./travel_recommendation.json')
+          .then(response => response.json())
+          .then(data => console.log(data))
+          .catch(error => console.error('Error:', error));
+      }
+
+function reset(){
+        const cleared document.getElementById("result") = "";
+      }
