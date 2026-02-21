4fd95691dfce54ca8a439c00a01b2ab29bf02d71
gosxiesperanto <g.toyota@gmail.com>
2026-02-20 19:33:52 -0500

    trying..

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,13 +1,23 @@
 function search(){
         const word = document.getElementById("txt").toLowerCase;
-        word.innerHTML = result;
-
+        word.innerHTML = result
+        console.log("txtは、", txt"
+")
         fetch('./travel_recommendation_api.json')
           .then(response => response.json())
-          .then(data => console.log(data))
-          .catch(error => console.error('Error:', error));
+          .then(data =>  {
+          if (!data.length) return;
+          
+          // ヘッダー生成（JSONのキーを自動取得）
+          const keys = Object.keys(data[0]);
+          document.getElementById('result').innerHTML = keys.value;
+          })
+          .catch(err => {
+          document.body.innerHTML += `<p style="color:red;">読み込みエラー: ${err.message}</p>`;
+          });          
       }
 
 function reset(){
     txt.value = "";
+    console.log("Successfully cleared");
         }
