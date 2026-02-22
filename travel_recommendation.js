a68d9b505c3166297921b78f75de9ec31dc64608
gosxiesperanto <g.toyota@gmail.com>
2026-02-22 02:44:39 -0500

    api

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -19,48 +19,20 @@ var url = './travel_recommendation_api.json';
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
 
-xhr.onload = console.log("Onload completed")
+xhr.onload = function() {
+    if (xhr.status === 200) {
+      // responseType='json'の場合、すでにオブジェクト化されている
+      var data = xhr.response; 
+      console.log(data);
 
-function onload1(){
-var countries = xhr.response.countries;
-var countriesDiv = document.getElementById('result');
-console.log(countriesDiv);
-countries.forEach(function(countries) {
-      var countriesDiv = document.createElement('div');
-      countriesDiv.classList.add('countries');
-
-      var name = document.createElement('h2');
-      name.textContent = countries.name;
-
-//      var waysHeader = document.createElement('h3');
-//      waysHeader.textContent = 'Ways to Achieve:';
-
-      var description = document.createElement('p');
-      description.textContent = countries.cities.description;
-
-//      var waysList = document.createElement('ul');
-//      article.ways_to_achieve.forEach(function(way) {
-//        var listItem = document.createElement('li');
-//        listItem.textContent = way;
-//        waysList.appendChild(listItem);
-//      });
-//      var benefitsHeader = document.createElement('h3');
-//      benefitsHeader.textContent = 'Benefits:';
-
-//      var benefitsList = document.createElement('ul');
-//      article.benefits.forEach(function(benefit) {
-//        var listItem = document.createElement('li');
-//        listItem.textContent = benefit;
-//       benefitsList.appendChild(listItem);
-//      }
-//    );
-
-    countriesDiv.appendChild(name);
-    countriesDiv.appendChild(cities.description);
-//    countriesDiv.appendChild(waysHeader);
-
-    countriesDiv.appendChild(countriesDiv);
-    });
-}
-
+      (function() {
+            console.log('beaches : ', data.beaches[0].name);
+            console.log('temples : ', data.temples[0].name);
+            console.log('countries : ', data.countries[0].id);
+            console.log('countries : ', data.countries[0].name);
+        }());
+    }
+  };
 xhr.send();
+
+
