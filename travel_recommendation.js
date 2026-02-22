a539ef058d9163079a5c318e93cd62ac967c3933
gosxiesperanto <g.toyota@gmail.com>
2026-02-22 06:51:34 -0500

    search

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,51 +3,27 @@ function search(){
     document.getElementById("txt").value = word;
     console.log(word);
 
-    var countries = xhr.response.countries;
-    var countriesDiv = document.getElementById('result');
-    
-    console.log("search done");
-        };
-
-function reset(){
-    txt.value = "";
-    console.log("Successfully cleared");
-        }
-
-var xhr = new XMLHttpRequest();
-var url = './travel_recommendation_api.json';
-xhr.open('GET', url, true);
-xhr.responseType = 'json';
-
-xhr.onload = function() {
-    if (xhr.status === 200) {
-      // responseType='json'の場合、すでにオブジェクト化されている
-      var data = xhr.response; 
-      console.log(data);
-
-      (function() {
-            console.log('beaches : ', data.beaches[0].name);
-            console.log('temples : ', data.temples[0].name);
-            console.log('countries : ', data.countries[0].id);
-            console.log('countries : ', data.countries[0].name);
-        }());
-    }
-  };
-xhr.send();
-
-
-fetch('./travel_recommendation_api.json')
-.then(response => response.json())
-.then(data => {
+    fetch('./travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
   // Handle successful data
 
-  const countries =  data.countries;
-  const temples = data.temples;
-  const beaches = data.beaches;
+        const countries = data.countries;
+        const temples = data.temples;
+        const beaches = data.beaches;
 
-  document.getElementById('result').innerHTML = beaches[0].name;
-  
-
+    console.log('FOR文の前 word:', word);
+  for (var i = 0; i < countries.length; i++){
+//    console.log('forで比べている対象の文字列',countries[i].name.toLowerCase());
+    if (word === countries[i].name.toLowerCase()) {
+        console.log('Word :', word);
+        console.log('matched!');
+        
+        document.getElementById('result').innerHTML = countries[i].description;
+        
+        return;
+        }
+    }
 })
 
 .catch(error => {
@@ -55,7 +31,13 @@ fetch('./travel_recommendation_api.json')
   console.error('Fetch error:', error.message);
 });
 
-    
+console.log("search done");
+};
+
+function reset(){
+    txt.value = "";
+    console.log("Successfully cleared");
+        }
 
 
 
