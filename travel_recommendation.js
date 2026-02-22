5b356dfbf28ef8cd989e113fe4902a39f8aa7472
gosxiesperanto <g.toyota@gmail.com>
2026-02-22 08:45:54 -0500

    search2

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,6 +3,9 @@ function search(){
     document.getElementById("txt").value = word;
     console.log(word);
 
+    document.getElementById('title').innerHTML = null;
+    document.getElementById('description').innerHTML = null;
+
     fetch('./travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
@@ -18,9 +21,20 @@ function search(){
     if (word === countries[i].name.toLowerCase()) {
         console.log('Word :', word);
         console.log('matched!');
-        
-        document.getElementById('result').innerHTML = countries[i].description;
-        
+
+        document.getElementById('title').innerHTML = countries[i].name;
+        var parent = document.getElementById('description');
+        var parent1 = document.getElementById('description1');
+
+        for (var j = 0; j < countries[i].cities.length; j++) {
+        const newItem = document.createElement('p');
+        newItem.innerHTML = countries[i].cities[j].name;
+        parent.appendChild(newItem);
+
+        const newItem1 = document.createElement('p');
+        newItem1.innerHTML = countries[i].cities[j].description;
+        parent1.appendChild(newItem1);
+        }
         return;
         }
     }
@@ -36,6 +50,8 @@ console.log("search done");
 
 function reset(){
     txt.value = "";
+    document.getElementById('title').innerHTML = null;
+    document.getElementById('description').innerHTML = null;
     console.log("Successfully cleared");
         }
 
